a9b31ab8b258c58f387c2b3c87bab37fa27bca33
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2018-10-11 11:17:20 +0200

    First commit. Server project: SpringCloudEurekaServer; Client project that creates the full sentence: SpringBootSentence; Order to start-up: SpringCloudEurekaServer, all client projects, and SpringBootSentence

---
+++ SpringCloudEurekaClientSubject/src/main/java/com/ab/SpringCloudEurekaClientSubject/SpringCloudEurekaClientSubjectApplication.java
@@ -0,0 +1,15 @@
+package com.ab.SpringCloudEurekaClientSubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudEurekaClientSubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaClientSubjectApplication.class, args);
+	}
+}
